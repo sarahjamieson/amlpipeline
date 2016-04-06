ab732b2562f29b5b9f97a01818349a5b34905173
sarahjamieson <sarah.jamieson@postgrad.manchester.ac.uk>
2016-04-06 12:16:16 +0100

    added qual graph

--- create_pdf.py
+++ create_pdf.py
@@ -75,5 +75,23 @@ class CreatePDF(object):
         return avg_qual
 
     def get_qual_graph(self, qualitymetrics):
-        cols_1_5 = range(0, 4)
-        print qualitymetrics.df[cols_1_5]
+        quality_df = qualitymetrics.df
+
+        cols_to_drop = ['cycle', 'lane', 'tile']
+        quality_df = quality_df.drop(cols_to_drop, axis=1)
+        qual_scores = quality_df.sum(axis=0).values
+        qual_list = map(int, qual_scores.tolist())
+        less_than_q30 = qual_list[0: 28]
+        more_than_q30 = qual_list[29: 50]
+
+        myint = 1000000
+        y1 = [x / myint for x in less_than_q30]
+        y2 = [x / myint for x in more_than_q30]
+        x1 = range(1, 29)
+        x2 = range(29, 50)
+
+        plt.bar(x1, y1, color='b')
+        plt.bar(x2, y2, color='g')
+        plt.ylabel('Clusters (millions)')
+        plt.xlabel('Q score')
+        plt.show()
